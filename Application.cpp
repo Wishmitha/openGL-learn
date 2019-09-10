50d03ef46a9f34fbf89485385638718f47729539
wishmitha <wishmitha.14@cse.mrt.ac.lk>
2019-09-10 22:33:10 +0800

    Added GLEW static lib

--- Application.cpp
+++ Application.cpp
@@ -1,4 +1,6 @@
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
+
 
 int main(void)
 {
@@ -7,6 +9,8 @@ int main(void)
 	/* Initialize the library */
 	if (!glfwInit())
 		return -1;
+
+	glewInit();
 
 	/* Create a windowed mode window and its OpenGL context */
 	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
